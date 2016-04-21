46bdf108ff3be61e95629bbb810d5e1fd6594ac4
jahdasha <jahdasha.flagg@gmail.com>
2016-04-20 21:42:48 -0400

    updated student.js

--- student.js
+++ student.js
@@ -1,7 +1,10 @@
 // Author: Jahdasha Flagg (Homework 11)
 
+	var prompt = require("prompt");
+	var Bus = require("./bus.js")
 	var Student = function (name, gender, grade, GPA, detentions, sleepingInClass, catchPhrase){
 
+		var thisStudent = this;
 		this.name = name;
 		this.gender = gender;
 		this.grade = grade;
@@ -9,12 +12,14 @@
 		this.detentions = detentions;
 		this.sleepingInClass =sleepingInClass;
 		this.catchPhrase = catchPhrase;
-		this.canStudentHaveFun = function(){
-		    if (this.detentions < 10 && this.GPA > 2){
+		this.canStudentHaveFun = function(detentions, GPA){
+		    if (thisStudent.detentions < 10 && thisStudent.GPA >= 2){
 		        console.log("The student can have fun. Go live it up!");
 		    }else{
 		        console.log("Go study, NOW !!!");
 		    }
 		}
+		canStudentHaveFun();
+	}
 
 	module.exports = Student;
