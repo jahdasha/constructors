cdd7533cbbb00825f98e2846bf58d8f765489185
Jahdasha Flagg <jahdashaflagg@Jahdashas-MBP.home>
2016-04-20 20:56:13 -0400

    updated student.js

--- student.js
+++ student.js
@@ -1,29 +1,20 @@
-// and a function called canStudentHaveFun that checks if they have less than 10 detentions 
-//and above a 2 GPA. If they do then console.log out to the terminal that the student can have fun.
+// Author: Jahdasha Flagg (Homework 11)
 
-// Prompt the user for a student's information, make a new Student Object with it, 
-// then invoke the canStudentHaveFun function.
+	var Student = function (name, gender, grade, GPA, detentions, sleepingInClass, catchPhrase){
 
-// export the student constructor at the bottom of the file
+		this.name = name;
+		this.gender = gender;
+		this.grade = grade;
+		this.GPA = GPA;
+		this.detentions = detentions;
+		this.sleepingInClass =sleepingInClass;
+		this.catchPhrase = catchPhrase;
+		this.canStudentHaveFun = function(){
+		    if (this.detentions < 10 && this.GPA > 2){
+		        console.log("The student can have fun. Go live it up!");
+		    }else{
+		        console.log("Go study, NOW !!!");
+		    }
+		}
 
-var Student = function (name, gender, grade, GPA, detentions, sleepingInClass, catchPhrase){
-
-this.name = name;
-this.gender = gender;
-this.grade = grade;
-this.GPA = GPA;
-this.detentions = detentions;
-this.sleepingInClass =sleepingInClass;
-this.catchPhrase = catchPhrase;
-
-var canStudentHaveFun = function(detentions, GPA){
-    if(detentions<10 && GPA >= 2){
-      console.log("Can have fun!");
-    }
-
-}
-
-
-}
-
-module.exports = Student;
+	module.exports = Student;
